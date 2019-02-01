78bf9544ab51b6e08579bbf214d8361772d411f3
Jusong Chen <jusong.chen@salesforce.com>
2019-01-31 19:16:19 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+[![Go Report Card](https://goreportcard.com/badge/github.com/rumyantseva/advent-2017)](https://goreportcard.com/report/github.com/rumyantseva/advent-2017)
+
+# advent-2017
+
+A set of examples for GopherAcademy Advent blog post:
+[Write a Kubernetes-ready service from zero step-by-step](https://blog.gopheracademy.com/advent-2017/kubernetes-ready-service). Russian version is [here](https://habrahabr.ru/post/345332/).
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/jusongchen/canis/handlers"
+	"github.com/jusongchen/canis/version"
+)
+
+// How to try it: PORT=8000 go run main.go
+func main() {
+	log.Printf(
+		"Starting the service...\ncommit: %s, build time: %s, release: %s",
+		version.Commit, version.BuildTime, version.Release,
+	)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not set.")
+	}
+
+	r := handlers.Router(version.BuildTime, version.Commit, version.Release)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: r,
+	}
+
+	// this channel is for graceful shutdown:
+	// if we receive an error, we can send it here to notify the server to be stopped
+	shutdown := make(chan struct{}, 1)
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			shutdown <- struct{}{}
+			log.Printf("%v", err)
+		}
+	}()
+	log.Print("The service is ready to listen and serve.")
+
+	select {
+	case killSignal := <-interrupt:
+		switch killSignal {
+		case os.Interrupt:
+			log.Print("Got SIGINT...")
+		case syscall.SIGTERM:
+			log.Print("Got SIGTERM...")
+		}
+	case <-shutdown:
+		log.Printf("Got an error...")
+	}
+
+	log.Print("The service is shutting down...")
+	srv.Shutdown(context.Background())
+	log.Print("Done")
+}
